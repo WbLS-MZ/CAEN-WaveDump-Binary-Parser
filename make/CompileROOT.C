dfb15403fd22ed90fc06aa759f6b45c41515cfbc
Dan Guff <daniele.guffanti@unimib.it>
2022-04-12 22:22:45 +0200

    Removed make build system

--- make/CompileROOT.C
+++
@@ -1,7 +0,0 @@
-{
-    gSystem->AddLinkedLibs( "-lboost_filesystem -lboost_system" );
-    gSystem->AddLinkedLibs( "-lcaenparse" );
-    gSystem->AddIncludePath( "-I../inc");
-    gROOT->LoadMacro( "../src/ROOT/CaenTreeCreator/CaenTreeCreator.cpp+" );
-    //gROOT->LoadMacro( "../src/ROOT/Drawer/Drawer.cpp+" );
-}
